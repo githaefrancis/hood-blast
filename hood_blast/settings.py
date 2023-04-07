fcaf7182e378ed8fcb30a0dd1cd38241fc583a25
Francis Githae <mureithigithae@gmail.com>
2023-04-07 10:55:36 +0300

    update settings

--- hood_blast/settings.py
+++ hood_blast/settings.py
@@ -103,7 +103,7 @@ DATABASES={
 
         'default':{
 
-            'ENGINE':'django.db.backends.postgresql',
+            'ENGINE':'django.db.backends.postgresql_psycopg2',
             'NAME':config('DB_NAME'),
             'USER':config('DB_USER'),
             'PASSWORD':config('DB_PASSWORD'),
@@ -132,8 +132,8 @@ DATABASES={
 #             default=config('DATABASE_URL')
 #         )
 #     }
-db_from_env=dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env=dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
